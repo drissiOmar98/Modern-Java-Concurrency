8d08954d8d5adb39999e0b54eccbedab8cdc8fab
DrissiOmar <omar.drissi@esprit.tn>
2026-09-05 22:50:26 +0100

    chore: set up Spring Boot multi-module demo for Virtual Threads and Structured Concurrency

---
+++ banking-data-service/src/test/java/com/omar/loomdemo/BankingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.loomdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ loan-origination-service/src/main/java/com/omar/loomdemo/LoanOriginationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omar.loomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanOriginationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanOriginationServiceApplication.class, args);
+	}
+
+}
